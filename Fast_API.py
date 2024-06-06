5ff0744f417670f58c7dcfbe0711f6197dfc0bd4
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2024-06-06 14:29:50 +0600

    Update Fast_API.py

--- Fast_API.py
+++ Fast_API.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
